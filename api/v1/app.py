91db8225ebe4572323395d0360b0e47180eeb313
tAf <muzirat0@gmail.com>
2024-04-30 20:05:31 +0200

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 """
 Flask entry point
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -11,9 +11,17 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def clean_up(e):
+    """ close storage """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """ error `404 """
+    return jsonify({"error": "Not found"})
 
 
 if __name__ == '__main__':
