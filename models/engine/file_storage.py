193b56b256268e730d210836717ef4933bfb090f
tAf <muzirat0@gmail.com>
2024-05-03 17:51:53 +0200

    engine from storage_get_count branch

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,17 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ retrieve one object """
+        objs = self.all(cls)
+        for key, val in objs.items():
+            if key.split('.')[1] == id:
+                return val
+            return None
+
+    def count(self, cls=None):
+        """ count the number of objects in storage """
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
