4b9eb80f5d2c6daa4875a5e44ae66848669c55dc
tAf <muzirat0@gmail.com>
2024-05-03 23:52:34 +0200

    6. State v0.1

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,8 +4,9 @@ states view
 """
 
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models import storage
+from models.state import State
 
 @app_views.route('/states', strict_slashes=False, methods=['GET'])
 def states():
@@ -35,3 +36,33 @@ def del_state(state_id):
         storage.save()
         return jsonify({}), 200
     abort(404)
+
+@app_views.route('/states', strict_slashes=False, methods=['POST'])
+def new_state():
+    """ creates state obj """
+    obj_data = request.get_json()
+    if not obj_data:
+        abort(400, 'Not a JSON')
+    if 'name' not in obj_data:
+        abort(400, 'Missing name')
+
+    obj = State(name=obj_data['name'])
+    storage.new(obj)
+    storage.save()
+
+    return obj.to_dict(), 201
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['PUT'])
+def update_state(state_id):
+    """ updates state """
+    obj_data = request.get_json()
+    if not obj_data:
+        abort(400, 'Not a JSON')
+    obj = storage.get('State', state_id)
+    ignore_keys = ["id", "created_at", "updated_at"]
+
+    for key, val in obj_data.items():
+        for i_key in ignore_keys:
+            if key != i_key:
+                setattr(obj, key, val)
+    return jsonify(obj.to_dict()), 200
